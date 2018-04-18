69ac8393b384e548d479edd2436ce73f5f6fa267
Sho Ishiguro <ishiguro.sho.grocio@gmail.com>
2018-04-18 15:12:11 +0900

    Create jmr.sh

---
+++ jmr.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo $1 |\
+  mecab -Oyomi |\
+  sed -e 's/。//g'\
+  -e 's/、//g'\
+  -e 's/ャ//g'\
+  -e 's/ュ//g'\
+  -e 's/ョ//g'\
+  -e 's/　//g'\
+  -e 's/ //g' |\
+  wc -m |
+  xargs -I{} echo {} - 1 |
+  bc
