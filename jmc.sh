b22072323d61c350e607b6b11337f8dd606898cd
grocio <sho.ishiguro.grocio@gmail.com>
2018-05-06 02:40:21 +0900

    Support for a pipeline and -y option

--- jmc.sh
+++ jmc.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
-echo $1 |
+y_flag=0
+
+while getopts 'y' option
+do
+  case $option in
+    y)
+      y_flag=1
+      ;;
+    \?)
+      echo 'Usage: jmc.sh [-y] target' 1>&2
+      exit 1
+      ;;
+  esac
+done
+
+shift $(expr $OPTIND - 1)
+
+_jmc() {
+if [ $y_flag -eq 1 ]; then
+  yomi=$(echo $1 | mecab -Oyomi)
+fi
+
+mora=$(echo $1 |
   mecab -Oyomi |
   sed -e 's/。//g'\
   -e 's/、//g'\
@@ -10,4 +32,20 @@ echo $1 |
   -e 's/ //g' |
   wc -m |
   xargs -I{} echo {} - 1 |
-  bc
+  bc)
+
+if [ $y_flag -eq 1 ]; then
+  echo "$yomi,$mora"
+else
+  echo "$mora"
+fi
+}
+
+if [ -p /dev/stdin ]; then
+  cat - | while read target
+  do
+    _jmc $target
+  done
+else
+  _jmc $1
+fi
